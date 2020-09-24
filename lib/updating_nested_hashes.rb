cf6283fda0a56af4e1d7033b5778a19388280abd
Roberto Acosta <x.rojo@hotmail.com>
2020-09-24 05:24:49 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -50,8 +50,12 @@ def add_characters
          {name: "Nurse", age: "44", attitude: "worried"}
          ]
       }
-      :
    }
+  epic_tragedy[:additional_characters] = [
+    {name: "Prince Escalus"},
+    {name: "Apothecary"}
+    ]
+    
 
    # Write code below that does the following programmatically:
    #
